2c9477d529f29a6634ca662d8dec793928041a89
younsoo-seong <s38268619@kakao.com>
2025-08-20 11:46:35 +0900

    프로필 수정

--- front-end/src/components/profile/Profile.jsx
+++ front-end/src/components/profile/Profile.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { BsPersonFill } from 'react-icons/bs';
 
 const Container = styled.div`
   display: flex;
@@ -37,7 +38,10 @@ const Profile = ({ setMyProfile }) => {
   return (
     <Container>
       {/* 프로필 이미지 */}
-      <div className="relative mr-4 w-[20%] rounded-full border-4 border-gray-300 shadow-md overflow-hidden mb-3">
+      <div
+        className="relative mr-3 w-24 h-24 rounded-full border-4 border-gray-300 shadow-md overflow-hidden mb-3
+              flex items-center justify-center"
+      >
         {userData.profileImage ? (
           <img
             src={userData.profileImage}
@@ -45,9 +49,8 @@ const Profile = ({ setMyProfile }) => {
             className="w-full h-full object-cover"
           />
         ) : (
-          <span className="text-5xl flex items-center justify-center h-full">
-            👤
-          </span>
+          // 아이콘에 m-auto 대신, 부모의 flex 속성으로 중앙 정렬됩니다.
+          <BsPersonFill size="80%" className="text-gray-500" />
         )}
       </div>
       <Box>
@@ -68,7 +71,7 @@ const Profile = ({ setMyProfile }) => {
               .map((pos) => (
                 <span
                   key={pos}
-                  className="bg-gray-100 px-2 py-1 rounded-full text-sm border border-gray-300"
+                  className="bg-gray-100 px-[clamp(4px,1vw,8px)] py-1 rounded-full text-sm border border-gray-300"
                 >
                   {pos}
                 </span>
